60a207c91491bb557220fad8b30e6b44930aefe8
Marco Ferrari <ferrari.marco@gmail.com>
2017-12-24 16:21:16 +0100

    Disable -x flag for CI scripts

--- test/scripts/ci/before_install.sh
+++ test/scripts/ci/before_install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -eux
+#!/bin/sh -eu
 
 wget https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip
 unzip -d packer packer_${PACKER_VERSION}_linux_amd64.zip
--- test/scripts/ci/install.sh
+++ test/scripts/ci/install.sh
@@ -1,3 +1,3 @@
-#!/bin/sh -eux
+#!/bin/sh -eu
 
 bundle install
