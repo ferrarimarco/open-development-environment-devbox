65cb85549860424f24316ac1ec59ddf0d279de84
Marco Ferrari <ferrari.marco@gmail.com>
2017-12-24 16:20:49 +0100

    Test Ansible role installation. Close #94

--- test/scripts/ci/script.sh
+++ test/scripts/ci/script.sh
@@ -1,8 +1,10 @@
-#!/bin/sh -eux
+#!/bin/sh -eu
+
+ROLE_FILE_PATH=provisioning/ansible/requirements.yml
+echo "Test Ansible role installation from $ROLE_FILE_PATH"
+ansible-galaxy install -r "$ROLE_FILE_PATH"
 
 # Disable this check until we have a feedback on https://github.com/hashicorp/packer/issues/5727
 #VAGRANT_CLOUD_TOKEN=dummy_token ./packer/packer validate ubuntu.json
-
-kitchen diagnose --all
 
 kitchen test
