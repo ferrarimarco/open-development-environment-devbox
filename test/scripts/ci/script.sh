d9400d5da46e929d2333ef1c7ef3d315e5b82eb5
Marco Ferrari <ferrari.marco@gmail.com>
2018-01-23 11:04:49 +0100

    Workaround to enable packer template validation

--- test/scripts/ci/script.sh
+++ test/scripts/ci/script.sh
@@ -1,10 +1,26 @@
 #!/bin/sh -eu
 
+mktouch() {
+  for f in "$@"; do
+    mkdir -p -- "$(dirname -- "$f")"
+    touch -a -- "$f"
+  done
+}
+
+
 ROLE_FILE_PATH=provisioning/ansible/requirements.yml
 echo "Test Ansible role installation from $ROLE_FILE_PATH"
 ansible-galaxy install -r "$ROLE_FILE_PATH"
 
-# Disable this check until we have a feedback on https://github.com/hashicorp/packer/issues/5727
-#VAGRANT_CLOUD_TOKEN=dummy_token ./packer/packer validate ubuntu.json
+# Workaround until we have feedback on https://github.com/hashicorp/packer/issues/5727
+mktouch \
+  ./builds/virtualbox-iso/ubuntu-17.10.1-amd64/open-development-environment-devbox-build.ovf \
+  ./builds/virtualbox-ovf/provision-install-ansible/ubuntu-17.10.1-amd64/open-development-environment-devbox-build.ovf \
+  ./builds/virtualbox-ovf/provision-ansible/ubuntu-17.10.1-amd64/open-development-environment-devbox-build.ovf \
+  ./builds/virtualbox-ovf/provision-cleanup/ubuntu-17.10.1-amd64/open-development-environment-devbox-build.ovf
+
+VAGRANT_CLOUD_TOKEN=dummy_token ./packer/packer validate ubuntu.json
 
 kitchen test
+
+rm -rf builds
