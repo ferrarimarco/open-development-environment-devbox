075800f803dd6ea7883ba31efba7aea11f1a8644
Marco Ferrari <ferrari.marco@gmail.com>
2018-03-14 15:55:35 +0100

    Update test for networking script

--- test/integration/script-networking/inspec/open-development-environment-devbox-script-networking.rb
+++ test/integration/script-networking/inspec/open-development-environment-devbox-script-networking.rb
@@ -10,4 +10,22 @@ control "open-development-environment-devbox-script-networking" do
     its('content') { should match('pre-up sleep 2') }
   end
 
+  describe file("/etc/default/grub") do
+    it { should exist }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_readable.by_user('root') }
+    its('mode') { should cmp '0644' }
+    its('content') { should match('GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0') }
+  end
+
+  describe file("/etc/netplan/01-netcfg.yaml") do
+    it { should exist }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    it { should be_readable.by_user('root') }
+    its('mode') { should cmp '0644' }
+    its('content') { should match('eth0') }
+  end
+
 end
