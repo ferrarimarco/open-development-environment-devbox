2a5b4c04a0058112cbed8180fcdc9bdac1482124
Marco Ferrari <ferrari.marco@gmail.com>
2018-03-28 15:33:52 +0200

    Move mount path to a variable

--- provisioning/scripts/virtualbox.sh
+++ provisioning/scripts/virtualbox.sh
@@ -8,13 +8,14 @@ virtualbox-iso|virtualbox-ovf)
     echo "==> Installing VirtualBox guest additions"
     VER=$(cat "$SSH_USER_HOME"/.vbox_version)
     ISO="VBoxGuestAdditions_$VER.iso";
-    mkdir -p /tmp/vbox;
-    mount -o loop" $SSH_USER_HOME/$ISO" /tmp/vbox;
-    sh /tmp/vbox/VBoxLinuxAdditions.run \
+    MOUNT_PATH="/tmp/vbox"
+    mkdir -p "$MOUNT_PATH";
+    mount -o loop" $SSH_USER_HOME/$ISO" "$MOUNT_PATH";
+    sh "$MOUNT_PATH"/VBoxLinuxAdditions.run \
         || echo "VBoxLinuxAdditions.run exited $? and is suppressed." \
             "For more read https://www.virtualbox.org/ticket/12479";
-    umount /tmp/vbox;
-    rm -rf /tmp/vbox;
+    umount "$MOUNT_PATH";
+    rm -rf "$MOUNT_PATH";
     rm -f "$SSH_USER_HOME"/*.iso;
     ;;
 esac
