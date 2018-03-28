7f82acfa2eb02a162cb3d2d35855d86080ff2329
Marco Ferrari <ferrari.marco@gmail.com>
2018-03-28 15:35:09 +0200

    Ensure unattended upgrade is disabled

--- provisioning/scripts/update.sh
+++ provisioning/scripts/update.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -eux
 
+ubuntu_version="$(lsb_release -r | awk '{print $2}')";
+major_version="$(echo "$ubuntu_version" | awk -F. '{print $1}')";
+
 echo "==> Setting DEBIAN_FRONTEND"
 export DEBIAN_FRONTEND=noninteractive
 
@@ -29,11 +32,30 @@ task
 exec /usr/bin/apt-get update
 EOF
 
+# Disable systemd apt timers/services
+if [ "$major_version" -ge "16" ]; then
+  systemctl stop apt-daily.timer;
+  systemctl stop apt-daily-upgrade.timer;
+  systemctl disable apt-daily.timer;
+  systemctl disable apt-daily-upgrade.timer;
+  systemctl mask apt-daily.service;
+  systemctl mask apt-daily-upgrade.service;
+  systemctl daemon-reload;
+fi
+
 # Disable periodic activities of apt
 echo "==> Disable periodic activities of apt"
 cat <<EOF >/etc/apt/apt.conf.d/10disable-periodic;
 APT::Periodic::Enable "0";
+APT::Periodic::Update-Package-Lists "0";
+APT::Periodic::Download-Upgradeable-Packages "0";
+APT::Periodic::AutocleanInterval "0";
+APT::Periodic::Unattended-Upgrade "0";
 EOF
+
+# Clean and nuke the package from orbit
+rm -rf /var/log/unattended-upgrades;
+apt-get -y purge unattended-upgrades;
 
 UPDATE=${UPDATE:-false}
 
