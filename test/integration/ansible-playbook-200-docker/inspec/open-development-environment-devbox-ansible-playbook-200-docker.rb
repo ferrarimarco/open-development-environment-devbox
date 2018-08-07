1b10cac7f5ca6e534a2c4029d9b08e0dfccf5726
Marco Ferrari <ferrari.marco@gmail.com>
2018-08-07 14:52:47 +0200

    Remove bits that will be containerized (1/2)

--- test/integration/ansible-playbook-200-docker/inspec/open-development-environment-devbox-ansible-playbook-200-docker.rb
+++
@@ -1,9 +0,0 @@
-control "open-development-environment-devbox-ansible-playbook-200-docker" do
-  title "open-development-environment-devbox-ansible-playbook-200-docker control"
-
-  describe user('vagrant') do
-    it { should exist }
-    its('groups') { should eq ['vagrant', 'docker']}
-  end
-
-end
--- test/scripts/ci/install-ansible-roles.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -eu
-
-ROLE_FILE_PATH=provisioning/ansible/requirements.yml
-echo "Test Ansible role installation from $ROLE_FILE_PATH"
-ansible-galaxy install -r "$ROLE_FILE_PATH"
