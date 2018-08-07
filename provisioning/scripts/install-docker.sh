c33cfae8613ded842584fc97da92b8c040c07719
Marco Ferrari <ferrari.marco@gmail.com>
2018-08-07 14:52:05 +0200

    Install docker-compose

--- provisioning/scripts/install-docker.sh
+++ provisioning/scripts/install-docker.sh
@@ -18,4 +18,7 @@ if which docker >/dev/null 2>&1 ; then
 else
   curl -sSL https://get.docker.com | sh
   usermod -aG docker "$user"
+
+  curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
 fi
